c9e6b055464062d9c577787b7daa4afd68778f76
jayveedees <jayveedees@gmail.com>
2019-06-02 13:23:01 +0200

    Started CDIO development

--- src/Data/Game/Actor.java
+++ src/Data/Game/Actor.java
@@ -1,4 +1,53 @@
 package Data.Game;
 
+import java.util.List;
+
 public class Actor {
+
+    private int actorid;
+    private String name;
+    private List<Character> charactersPlayed;
+    private List<Game> gamesStarredIn;
+
+    public Actor(int actorid, String name, List<Character> charactersPlayed, List<Game> gamesStarredIn) {
+        this.actorid = actorid;
+        this.name = name;
+        this.charactersPlayed = charactersPlayed;
+        this.gamesStarredIn = gamesStarredIn;
+    }
+
+    public Actor() {
+    }
+
+    public int getActorid() {
+        return actorid;
+    }
+
+    public void setActorid(int actorid) {
+        this.actorid = actorid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Character> getCharactersPlayed() {
+        return charactersPlayed;
+    }
+
+    public void setCharactersPlayed(List<Character> charactersPlayed) {
+        this.charactersPlayed = charactersPlayed;
+    }
+
+    public List<Game> getGamesStarredIn() {
+        return gamesStarredIn;
+    }
+
+    public void setGamesStarredIn(List<Game> gamesStarredIn) {
+        this.gamesStarredIn = gamesStarredIn;
+    }
 }
