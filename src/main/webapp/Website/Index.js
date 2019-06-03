8847d844a7390f3ce196144a62a6ddbce23fd4eb
Duyx <sandersen98@live.dk>
2019-06-03 12:56:30 +0200

    prøv igen

--- src/main/webapp/Website/Index.js
+++ src/main/webapp/Website/Index.js
@@ -1,14 +1,22 @@
-const slideShowSlide = document.querySelector('.slideshow-container');
-const slideImages = document.querySelectorAll('.slideshow-slide');
+const slideShowSlide = document.querySelector('.slideshow-slide');
+const slideImages = document.querySelectorAll('.slideshow-slide img');
 
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
 
 let counter = 1;
-const size = slideShowSlide[0].clientWidth;
+const size = slideImages[0].clientWidth;
 
 slideShowSlide.style.transform = 'tranlateX(' + (-size * counter) +  'px)';
 
-nextBtn.addEventListener('click',()=>{
+nextBtn.addEventListener('click',() => {
     slideShowSlide.style.transition = "transform 0.4s ease-in-out";
-}
+    counter++;
+    slideShowSlide.style.transform = 'tranlateX(' + (-size * counter) +  'px)';
+});
+
+prevBtn.addEventListener('click',() => {
+    slideShowSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    slideShowSlide.style.transform = 'tranlateX(' + (-size * counter) +  'px)';
+});
