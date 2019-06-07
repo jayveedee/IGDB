7b4a952983372e3f893fcdefcb768d6328ca0b76
jayveedees <jayveedees@gmail.com>
2019-06-07 18:49:07 +0200

    Slettet DAO klasser, fordi for meget arbejde til dette projekt. Laver om på klasser også

--- src/main/java/Data/GameDTO/GameDTO.java
+++ src/main/java/Data/GameDTO/GameDTO.java
@@ -19,22 +19,13 @@ public class GameDTO {
     private DeveloperDTO            gameDEV;        private PublisherDTO            gamePUB;
     private ComposerDTO             gameCOMP;       private SoundtrackDTO           gameOST;
 
-    private int                     gameAGERATING;  private String                  gameCover;
-    private String                  gameBIO;        private String                  gameBG;
-    private List<MediaDTO>          gameMEDIADTO;
+    private String                  gameCover;      private String                  gameBG;
+    private String                  gameBIO;        private List<MediaDTO>          gameMEDIADTO;
 
-    public GameDTO
-            (
-                int gameID, String gameNAME,
-                List<Integer> gameCHs, List<Integer> gameACs,
-                List<Integer> gameGENREs, List<Integer> gameGMs,
-                List<Integer> gameRATINGs, DateDTO gameRELEASEDATE,
-                WriterDTO gameWRI,
-                DeveloperDTO gameDEV, PublisherDTO gamePUB,
-                ComposerDTO gameCOMP, SoundtrackDTO gameOST,
-                String gameBIO, int gameAGERATING,
-                List<MediaDTO> gameMEDIADTO)
-            {
+    public GameDTO() {
+    }
+
+    public GameDTO(int gameID, String gameNAME, List<Integer> gameCHs, List<Integer> gameACs, List<Integer> gameGENREs, List<Integer> gameGMs, List<Integer> gameRATINGs, DateDTO gameRELEASEDATE, WriterDTO gameWRI, DeveloperDTO gameDEV, PublisherDTO gamePUB, ComposerDTO gameCOMP, SoundtrackDTO gameOST, String gameCover, String gameBG, String gameBIO, List<MediaDTO> gameMEDIADTO) {
         this.gameID = gameID;
         this.gameNAME = gameNAME;
         this.gameCHs = gameCHs;
@@ -48,107 +39,144 @@ public class GameDTO {
         this.gamePUB = gamePUB;
         this.gameCOMP = gameCOMP;
         this.gameOST = gameOST;
+        this.gameCover = gameCover;
+        this.gameBG = gameBG;
         this.gameBIO = gameBIO;
-        this.gameAGERATING = gameAGERATING;
         this.gameMEDIADTO = gameMEDIADTO;
-    }
-
-    public GameDTO() {
     }
 
     public int getGameID() {
         return gameID;
     }
+
     public void setGameID(int gameID) {
         this.gameID = gameID;
     }
+
     public String getGameNAME() {
         return gameNAME;
     }
+
     public void setGameNAME(String gameNAME) {
         this.gameNAME = gameNAME;
     }
+
     public List<Integer> getGameCHs() {
         return gameCHs;
     }
+
     public void setGameCHs(List<Integer> gameCHs) {
         this.gameCHs = gameCHs;
     }
+
     public List<Integer> getGameACs() {
         return gameACs;
     }
+
     public void setGameACs(List<Integer> gameACs) {
         this.gameACs = gameACs;
     }
+
     public List<Integer> getGameGENREs() {
         return gameGENREs;
     }
+
     public void setGameGENREs(List<Integer> gameGENREs) {
         this.gameGENREs = gameGENREs;
     }
+
     public List<Integer> getGameGMs() {
         return gameGMs;
     }
+
     public void setGameGMs(List<Integer> gameGMs) {
         this.gameGMs = gameGMs;
     }
+
     public List<Integer> getGameRATINGs() {
         return gameRATINGs;
     }
+
     public void setGameRATINGs(List<Integer> gameRATINGs) {
         this.gameRATINGs = gameRATINGs;
     }
+
     public DateDTO getGameRELEASEDATE() {
         return gameRELEASEDATE;
     }
+
     public void setGameRELEASEDATE(DateDTO gameRELEASEDATE) {
         this.gameRELEASEDATE = gameRELEASEDATE;
     }
+
     public WriterDTO getGameWRI() {
         return gameWRI;
     }
+
     public void setGameWRI(WriterDTO gameWRI) {
         this.gameWRI = gameWRI;
     }
+
     public DeveloperDTO getGameDEV() {
         return gameDEV;
     }
+
     public void setGameDEV(DeveloperDTO gameDEV) {
         this.gameDEV = gameDEV;
     }
+
     public PublisherDTO getGamePUB() {
         return gamePUB;
     }
+
     public void setGamePUB(PublisherDTO gamePUB) {
         this.gamePUB = gamePUB;
     }
+
     public ComposerDTO getGameCOMP() {
         return gameCOMP;
     }
+
     public void setGameCOMP(ComposerDTO gameCOMP) {
         this.gameCOMP = gameCOMP;
     }
+
     public SoundtrackDTO getGameOST() {
         return gameOST;
     }
+
     public void setGameOST(SoundtrackDTO gameOST) {
         this.gameOST = gameOST;
     }
+
+    public String getGameCover() {
+        return gameCover;
+    }
+
+    public void setGameCover(String gameCover) {
+        this.gameCover = gameCover;
+    }
+
+    public String getGameBG() {
+        return gameBG;
+    }
+
+    public void setGameBG(String gameBG) {
+        this.gameBG = gameBG;
+    }
+
     public String getGameBIO() {
         return gameBIO;
     }
+
     public void setGameBIO(String gameBIO) {
         this.gameBIO = gameBIO;
     }
-    public int getGameAGERATING() {
-        return gameAGERATING;
-    }
-    public void setGameAGERATING(int gameAGERATING) {
-        this.gameAGERATING = gameAGERATING;
-    }
+
     public List<MediaDTO> getGameMEDIADTO() {
         return gameMEDIADTO;
     }
+
     public void setGameMEDIADTO(List<MediaDTO> gameMEDIADTO) {
         this.gameMEDIADTO = gameMEDIADTO;
     }
