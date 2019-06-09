cc6945108019319b79c4cf276ec31311eb595ca9
jayveedees <jayveedees@gmail.com>
2019-06-09 16:17:42 +0200

    I EM CONFUSED LUL

--- src/main/java/Data/GameDAL/GameDAO.java
+++ src/main/java/Data/GameDAL/GameDAO.java
@@ -37,7 +37,7 @@ public class GameDAO implements IGameDAO {
         List<PictureDTO>    gamePIC = game.getGamePICs();               List<RatingDTO>     gameRATING = game.getGameRATINGs();
         List<TrailerDTO>    gameTRAILER = game.getGameTRAILERs();
         DeveloperDTO        gameDEV = game.getGameDEV();                PublisherDTO        gamePUB = game.getGamePUB();
-        WriterDTO           gameWTI = game.getGameWRI();
+        WriterDTO           gameWRI = game.getGameWRI();
         ComposerDTO         gameCOMP = game.getGameCOMP();              SoundtrackDTO       gameOST = game.getGameOST();
 
         handleINSERTGame                        (query, gameID, gameTitle, gameDESC, gameRDstring, gameCOV, gameBG);
@@ -48,10 +48,82 @@ public class GameDAO implements IGameDAO {
         handleINSERTPictures                    (gameID, gamePIC);
         handleINSERTTrailers                    (gameID, gameTRAILER);
         handleINSERTDeveloperXParentCompany     (gameID, gameDEV);
+        handleINSERTPublisher                   (gameID, gamePUB);
+        handleINSERTWriter                      (gameID, gameWRI);
+        handleINSERTComposer                    (gameID, gameCOMP);
+        if (gameOST != null){
+            String queryARTIST = "";
+            String queryOST = "";
+        }
 
         return true;
     }
 
+    private boolean handleINSERTComposer(int gameID, ComposerDTO gameCOMP) {
+        if (gameCOMP != null){
+            String queryCOMP =
+                "INSERT INTO ComposerList (compID, compFN, compLN, compGameID) " +
+                "VALUES (?, ?, ?, ?)";
+            int             compID              = gameCOMP.getCompID();
+            String          compFN              = gameCOMP.getCompFN();
+            String          compLN              = gameCOMP.getCompLN();
+            return (handleINSERT_fn_ln_gameid(gameID, queryCOMP, compID, compFN, compLN));
+        }
+        return false;
+    }
+    private boolean handleINSERTWriter(int gameID, WriterDTO gameWRI) {
+        if (gameWRI != null) {
+            String queryWRI =
+                "INSERT INTO WriterList (writerID, writerFN, writerLN, writerGameID) " +
+                "VALUES (?, ?, ?, ?)";
+            int                 writerID            = gameWRI.getWriterID();
+            String              writerFN            = gameWRI.getWriterFN();
+            String              writerLN            = gameWRI.getWriterLN();
+            return handleINSERT_fn_ln_gameid(gameID, queryWRI, writerID, writerFN, writerLN);
+        }
+        return false;
+    }
+    private boolean handleINSERT_fn_ln_gameid(int gameID, String query, int ID, String FN, String LN) {
+        try {
+            handleINSERTDuplicateCode2(query, ID, FN, LN);
+            mySql.getPrepStatement().setInt(4,gameID);
+            mySql.getPrepStatement().executeUpdate();
+            mySql.getConnection().commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    private void handleINSERTDuplicateCode2(String query, int ID, String FN_or_NAME, String LN_or_CREATEDstring) throws SQLException {
+        mySql.getConnection().setAutoCommit(false);
+        mySql.setPrepStatment(mySql.getConnection().prepareStatement(query));
+        mySql.getPrepStatement().setInt(1, ID);
+        mySql.getPrepStatement().setString(2, FN_or_NAME);
+        mySql.getPrepStatement().setString(3, LN_or_CREATEDstring);
+    }
+    private void handleINSERTPublisher(int gameID, PublisherDTO gamePUB) {
+        if (gamePUB != null) {
+            String queryPUB =
+                "INSERT INTO PublisherList (pubID, pubNAME, pubCREATED, pubCOUNTRY, pubSTATUS, pubGameID) " +
+                "VALUES (?, ?, ?, ?, ?, ?)";
+            int             pubID               = gamePUB.getPubID();
+            String          pubNAME             = gamePUB.getPubNAME();
+            DateDTO pubCREATED          = gamePUB.getPubCREATED();
+                String      pubCREATEDstring    = pubCREATED.getDay() + "/" + pubCREATED.getMonth() + "/" +pubCREATED.getYaer();
+            String          pubCOUNTRY          = gamePUB.getPubCOUNTRY();
+            boolean         pubSTATUS           = gamePUB.isPubSTATUS();
+
+            try {
+                handleINSERTCompanyDuplicateCode1(queryPUB,pubID,pubNAME,pubCREATEDstring,pubCOUNTRY,pubSTATUS);
+                mySql.getPrepStatement().setInt(6,gameID);
+                mySql.getPrepStatement().executeUpdate();
+                mySql.getConnection().commit();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     private boolean handleINSERTDeveloperXParentCompany(int gameID, DeveloperDTO gameDEV) {
         if (gameDEV != null) {
             String queryParent =
@@ -74,11 +146,11 @@ public class GameDAO implements IGameDAO {
             String      devCOUNTRY          = gameDEV.getDevCOUNTRY();
             boolean     devSTATUS           = gameDEV.isDevSTATUS();
             try {
-                handleINSERTCompanyDuplicateCode(queryParent, parentID, parentNAME, parentCREATEDstring, parentCOUNTRY, parentSTATUS);
+                handleINSERTCompanyDuplicateCode1(queryParent, parentID, parentNAME, parentCREATEDstring, parentCOUNTRY, parentSTATUS);
                 mySql.getPrepStatement().executeUpdate();
                 mySql.getConnection().commit();
 
-                handleINSERTCompanyDuplicateCode(queryDEV, devID, devNAME, devCREATEDstring, devCOUNTRY, devSTATUS);
+                handleINSERTCompanyDuplicateCode1(queryDEV, devID, devNAME, devCREATEDstring, devCOUNTRY, devSTATUS);
                 mySql.getPrepStatement().setInt(6,parentID);
                 mySql.getPrepStatement().setInt(7,gameID);
                 mySql.getPrepStatement().executeUpdate();
@@ -90,21 +162,14 @@ public class GameDAO implements IGameDAO {
         }
         return true;
     }
-
-    private void handleINSERTCompanyDuplicateCode(String query, int companyID, String companyNAME, String companyCREATED, String companyCOUNTRY, boolean companySTATUS) throws SQLException {
+    private void handleINSERTCompanyDuplicateCode1(String query, int companyID, String companyNAME, String companyCREATED, String companyCOUNTRY, boolean companySTATUS) throws SQLException {
         handleINSERTDuplicateCode(query, companyID, companyNAME, companyCREATED, companyCOUNTRY);
         mySql.getPrepStatement().setBoolean(5, companySTATUS);
     }
-
     private void handleINSERTDuplicateCode(String query, int ID, String NAME, String CREATED, String COUNTRY_or_DESC) throws SQLException {
-        mySql.getConnection().setAutoCommit(false);
-        mySql.setPrepStatment(mySql.getConnection().prepareStatement(query));
-        mySql.getPrepStatement().setInt(1, ID);
-        mySql.getPrepStatement().setString(2, NAME);
-        mySql.getPrepStatement().setString(3, CREATED);
+        handleINSERTDuplicateCode2(query, ID, NAME, CREATED);
         mySql.getPrepStatement().setString(4, COUNTRY_or_DESC);
     }
-
     private boolean handleINSERTTrailers(int gameID, List<TrailerDTO> gameTRAILER) {
         if (!gameTRAILER.isEmpty()){
             String queryTRAILER =
@@ -128,7 +193,6 @@ public class GameDAO implements IGameDAO {
         }
         return true;
     }
-
     private boolean handleINSERTPictures(int gameID, List<PictureDTO> gamePIC) {
         if (!gamePIC.isEmpty()){
             String queryPICTURES =
@@ -152,7 +216,6 @@ public class GameDAO implements IGameDAO {
         }
         return true;
     }
-
     private boolean handleINSERTGameModes(int gameID, List<GameModeDTO> gameGM) {
         if (!gameGM.isEmpty()) {
             String queryGameGM =
@@ -176,7 +239,6 @@ public class GameDAO implements IGameDAO {
         }
         return true;
     }
-
     private boolean handleINSERTGenres(int gameID, List<GenreDTO> gameGENRE) {
         if (!gameGENRE.isEmpty()) {
             String queryGENRE =
@@ -200,7 +262,6 @@ public class GameDAO implements IGameDAO {
         }
         return true;
     }
-
     private boolean handleINSERTGame(String query1, int gameID, String gameTitle, String gameDESC, String gameReleaseString, String gameCOV, String gameBG) {
         try {
             handleINSERTDuplicateCode(query1, gameID, gameTitle, gameReleaseString, gameDESC);
@@ -212,7 +273,6 @@ public class GameDAO implements IGameDAO {
         }
         return true;
     }
-
     //FIXME måske kan opstå en fejl ved indsætning af flere spil til denne actor, ikke helt sikker (TEST SENERE)
     private boolean handleINSERTActors(int gameID, List<ActorDTO> gameACTOR, List<CharacterDTO> gameCHAR) {
         if (!gameACTOR.isEmpty() && !gameCHAR.isEmpty()) {
@@ -246,7 +306,6 @@ public class GameDAO implements IGameDAO {
         }
         return true;
     }
-
     private boolean handleINSERTCharacters(int gameID, List<CharacterDTO> gameCHAR) {
         if (!gameCHAR.isEmpty()) {
             String queryCHAR =
