c268686caa68756d8c2bdad2b50bca4a5b737a6e
Duyx <sandersen98@live.dk>
2019-06-11 20:54:16 +0200

    Article ting commit, What do you call a funny mountain? hill-arious.... nej? fair nok
    eeeehhhhhhhhh..... btich

--- src/main/webapp/Website/Functions/createarticle.js
+++ src/main/webapp/Website/Functions/createarticle.js
@@ -1,3 +1,4 @@
+/*--------------------------------------------------------------------------bt1*/
 $(document).on("click", ".btn-add-row", function(){
     var row = $(".row").eq(0).clone().show();
 
@@ -9,7 +10,7 @@ $(document).on("click", ".btn-remove-row", function(){
    $(".row").eq(index).remove();
 });
 
-
+/*--------------------------------------------------------------------------bt2*/
 $(document).on("click", ".btn-add-row2", function(){
     var row2 = $(".row2").eq(0).clone().show();
 
@@ -20,7 +21,7 @@ $(document).on("click", ".btn-remove-row2", function(){
     var index2 = $(".btn-remove-row2").index(this);
     $(".row2").eq(index2).remove();
 });
-
+/*--------------------------------------------------------------------------bt3*/
 $(document).on("click", ".btn-add-row3", function(){
     var row3 = $(".row3").eq(0).clone().show();
 
@@ -30,4 +31,15 @@ $(document).on("click", ".btn-add-row3", function(){
 $(document).on("click", ".btn-remove-row3", function(){
     var index3 = $(".btn-remove-row3").index(this);
     $(".row3").eq(index3).remove();
+});
+/*--------------------------------------------------------------------------bt4*/
+$(document).on("click", ".btn-add-row4", function(){
+    var row4 = $(".row4").eq(0).clone().show();
+
+    $(".element-wrapper4").append(row4)
+});
+
+$(document).on("click", ".btn-remove-row4", function(){
+    var index4 = $(".btn-remove-row4").index(this);
+    $(".row4").eq(index4).remove();
 });
