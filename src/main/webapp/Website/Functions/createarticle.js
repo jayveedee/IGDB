a31ceebc1a51556dec1e51183887a076e0a26e68
Asama <asamahayder@yahoo.dk>
2019-06-14 16:45:37 +0200

    mere arbejde på createGame

--- src/main/webapp/Website/Functions/createarticle.js
+++ src/main/webapp/Website/Functions/createarticle.js
@@ -1,93 +1,147 @@
+var row11counter = 0;
+var row9counter = 0;
 var row8counter = 0;
+var row7counter = 0;
+var row6counter = 0;
+var row5counter = 0;
+var row3Counter = 0;
+var row2Counter = 0;
+var row1Counter = 0;
 
 /*--------------------------------------------------------------------------bt1*/
 $(document).on("click", ".btn-add-row", function(){
-    var row = $(".row").eq(0).clone().show();
-    $(".element-wrapper").append(row)
+    row1Counter++;
+    var rowName = $(".rowName").eq(0).clone().show();
+    rowName.attr("id","rowName-"+row1Counter);
+    $(".element-wrapper").append(rowName);
+
+    var rowURL = $(".rowURL").eq(0).clone().show();
+    rowURL.attr("id","rowURL-"+row1Counter);
+    $(".element-wrapper").append(rowURL);
+
+    var rowBirthDay = $(".rowBirthDay").eq(0).clone().show();
+    rowBirthDay.attr("id","rowBirthDay-"+row1Counter);
+    $(".element-wrapper").append(rowBirthDay);
 });
 
 $(document).on("click", ".btn-remove-row", function(){
-   var index = $(".btn-remove-row").index(this);
-   $(".row").eq(index).remove();
+   if (row1Counter !== 0){
+       $(".rowName").eq(row1Counter).remove();
+       $(".rowURL").eq(row1Counter).remove();
+       $(".rowBirthDay").eq(row1Counter).remove();
+       row1Counter--;
+   }
 });
 
 /*--------------------------------------------------------------------------bt2*/
 $(document).on("click", ".btn-add-row2", function(){
+    row2Counter++;
     var row2 = $(".row2").eq(0).clone().show();
-    $(".element-wrapper2").append(row2)
+    row2.attr("id","row2-"+row2Counter);
+    $(".element-wrapper2").append(row2);
 });
 
 $(document).on("click", ".btn-remove-row2", function(){
-    var index2 = $(".btn-remove-row2").index(this);
-    $(".row2").eq(index2).remove();
+    if (row2Counter !== 0) {
+        $(".row2").eq(row2Counter).remove();
+        row2Counter--;
+    }
 });
 /*--------------------------------------------------------------------------bt3*/
 $(document).on("click", ".btn-add-row3", function(){
+    row3Counter++;
     var row3 = $(".row3").eq(0).clone().show();
-    $(".element-wrapper3").append(row3)
+    row3.attr("id","row3-"+row3Counter);
+    $(".element-wrapper3").append(row3);
 });
 
 $(document).on("click", ".btn-remove-row3", function(){
-    var index3 = $(".btn-remove-row3").index(this);
-    $(".row3").eq(index3).remove();
+    if (row3Counter !== 0) {
+        var index3 = $(".btn-remove-row3").index(this);
+        $(".row3").eq(row3Counter).remove();
+        row3Counter--;
+    }
 });
 
 /*--------------------------------------------------------------------------bt5*/
 $(document).on("click", ".btn-add-row5", function(){
-    var row5 = $(".row5").eq(0).clone().show();
-    $(".element-wrapper5").append(row5)
+    row5counter++;
+    var row5name = $(".row5name").eq(0).clone().show();
+    row5name.attr("id","row5name-"+row5counter);
+    $(".element-wrapper5").append(row5name);
+
+    var row5URL = $(".row5URL").eq(0).clone().show();
+    row5URL.attr("id","row5URL-"+row5counter);
+    $(".element-wrapper5").append(row5URL);
 });
 
 $(document).on("click", ".btn-remove-row5", function(){
-    var index5 = $(".btn-remove-row5").index(this);
-    $(".row5").eq(index5).remove();
+    if (row5counter !== 0){
+        $(".row5name").eq(row5counter).remove();
+
+        $(".row5URL").eq(row5counter).remove();
+        row5counter--;
+    }
 });
 /*--------------------------------------------------------------------------bt6*/
 $(document).on("click", ".btn-add-row6", function(){
+    row6counter++;
     var row6 = $(".row6").eq(0).clone().show();
-    $(".element-wrapper6").append(row6)
+    row6.attr("id","row6-"+row6counter);
+    $(".element-wrapper6").append(row6);
 });
 
 $(document).on("click", ".btn-remove-row6", function(){
-    var index6 = $(".btn-remove-row6").index(this);
-    $(".row6").eq(index6).remove();
+    if (row6counter !== 0) {
+        $(".row6").eq(row6counter).remove();
+        row6counter--;
+    }
 });
 /*--------------------------------------------------------------------------bt7*/
 $(document).on("click", ".btn-add-row7", function(){
+    row7counter++;
     var row7 = $(".row7").eq(0).clone().show();
-    $(".element-wrapper7").append(row7)
+    row7.attr("id","row7-"+row7counter);
+    $(".element-wrapper7").append(row7);
 });
 
 $(document).on("click", ".btn-remove-row7", function(){
-    var index7 = $(".btn-remove-row7").index(this);
-    $(".row7").eq(index7).remove();
+    if (row7counter !== 0) {
+        $(".row7").eq(row7counter).remove();
+        row7counter--;
+    }
 });
 /*--------------------------------------------------------------------------bt8*/
 $(document).on("click", ".btn-add-row8", function(){
     row8counter++;
     var row8 = $(".row8").eq(0).clone().show();
-    row8.attr("id","row8"+row8counter);
+    row8.attr("id","row8-"+row8counter);
     $(".element-wrapper8").append(row8);
 });
 
 $(document).on("click", ".btn-remove-row8", function(){
+    var index8 = $(".btn-remove-row8").index(this);
+    $(".row8").eq(row8counter).remove();
     if (row8counter !== 0){
         row8counter--;
     }
-    var index8 = $(".btn-remove-row8").index(this);
-    $(".row8").eq(index8).remove();
 });
 /*--------------------------------------------------------------------------bt9*/
 $(document).on("click", ".btn-add-row9", function(){
+    row9counter++;
     var row9 = $(".row9").eq(0).clone().show();
+    row9.attr("id","row9-",row9counter);
     $(".element-wrapper9").append(row9)
 });
 
 $(document).on("click", ".btn-remove-row9", function() {
-    var index9 = $(".btn-remove-row9").index(this);
-    $(".row9").eq(index9).remove();
+    if (row9counter !== 0) {
+        $(".row9").eq(row9counter).remove();
+        row9counter--;
+    }
 });
 /*--------------------------------------------------------------------------bt10*/
+//BLIVER DENNE HER IKKE BRUGT??? I SÅ FALD SKAL DEN SLETTES
 $(document).on("click", "#btn10", function(){
     var row10 = $(".row10").eq(0).clone().show();
     $(".element-wrapper10").append(row10)
@@ -99,13 +153,22 @@ $(document).on("click", ".btn-remove-row10", function() {
 });
 /*--------------------------------------------------------------------------bt11*/
 $(document).on("click", "#btn11", function(){
-    var row11 = $(".row11").eq(0).clone().show();
-    $(".element-wrapper11").append(row11)
+    row11counter++;
+    var row11Name = $(".row11Name").eq(0).clone().show();
+    row11Name.attr("id","row11Name-"+row11counter);
+    $(".element-wrapper11").append(row11Name);
+
+    var row11URL = $(".row11URL").eq(0).clone().show();
+    row11URL.attr("id","row11URL-"+row11counter);
+    $(".element-wrapper11").append(row11URL);
 });
 
 $(document).on("click", ".btn-remove-row11", function() {
-    var index11 = $(".btn-remove-row11").index(this);
-    $(".row11").eq(index11).remove();
+    if (row11counter !== 0) {
+        $(".row11Name").eq(row11counter).remove();
+        $(".row11URL").eq(row11counter).remove();
+        row11counter--;
+    }
 });
 
 
@@ -113,44 +176,8 @@ $("#createGameForm").submit(function (event) {
     event.preventDefault();
     //initializing game
 
-    var parentCompany = {
-        parentID : 0,
-        parentNAME : $("#companyNameField").val(),
-        parentCREATED : $("#creationOfCompanyField").val(),
-        parentCountry : $("#companyOriginField").val(),
-        parentSTATUS : $("#companyStatusField").val()
-    };
-
-    var developer = {
-        devID : 0,
-        devNAME : $("#developerNameField").val(),
-        devCREATED : $("#developerCreationField").val(),
-        devSTATUS : $("#developerStatusField").val(),
-        devCOUNTRY : $("#developerOriginField").val(),
-        devPCOMPANY : parentCompany,
-        devGAME : null
-    };
-
-    var publisher = {
-        pubID : 0,
-        pubNAME : $("#publisherNameField").val(),
-        pubCREATED : $("#publisherCreationField").val(),
-        pubCOUNTRY : $("#publisherOriginField").val(),
-        pubSTATUS : $("#publisherStatusField").val(),
-        pubGAME : null
-    };
-
-    var soundtrack = {
-        ostID : 0,
-        ostTITLE : $("#soundtrackNameField").val(),
-        ostCOMP : null,
-        ostMA : null,
-        ostGAME : 0,
-        ostURL : $("#soundtrackURLField").val()
-    };
-
     var game ={
-        gameID : 0,
+        gameID : generateRandomID(),
         gameNAME : $("#titlefield").val(),
         gameCHs : [],
         gameACs : [],
@@ -159,16 +186,52 @@ $("#createGameForm").submit(function (event) {
         gameRATINGs : [],
         gameRELEASEDATE : $("#releaseDateField").val(),
         gameWRI : [],
-        gameCOMP : parentCompany,
-        gameDEV : developer,
-        gamePUB : publisher,
-        gameOST : soundtrack,
+        gameCOMP : null,
+        gameDEV : null,
+        gamePUB : null,
+        gameOST : null,
         gamePLAT : [],
         gameCOVER : $("#picturefield").val(),
         gameBG : $("#backpicfield").val(),
         gameBIO : $("#gameDescfield").val(),
         gameTRAILERs : [],
         gamePICs : []
+    };
+
+    var parentCompany = {
+        parentID : generateRandomID(),
+        parentNAME : $("#companyNameField").val(),
+        parentCREATED : $("#creationOfCompanyField").val(),
+        parentCountry : $("#companyOriginField").val(),
+        parentSTATUS : $("#companyStatusField").val()
+    };
+
+    var developer = {
+        devID : generateRandomID(),
+        devNAME : $("#developerNameField").val(),
+        devCREATED : $("#developerCreationField").val(),
+        devSTATUS : $("#developerStatusField").val(),
+        devCOUNTRY : $("#developerOriginField").val(),
+        devPCOMPANY : parentCompany,
+        devGAME : game.gameID
+    };
+
+    var publisher = {
+        pubID : generateRandomID(),
+        pubNAME : $("#publisherNameField").val(),
+        pubCREATED : $("#publisherCreationField").val(),
+        pubCOUNTRY : $("#publisherOriginField").val(),
+        pubSTATUS : $("#publisherStatusField").val(),
+        pubGAME : game.gameID
+    };
+
+    var soundtrack = {
+        ostID : generateRandomID(),
+        ostTITLE : $("#soundtrackNameField").val(),
+        ostCOMP : null,
+        ostMA : null,
+        ostGAME : game.gameID,
+        ostURL : $("#soundtrackURLField").val()
     };
 
     var platform={
@@ -221,12 +284,6 @@ $("#createGameForm").submit(function (event) {
         acPFP : "placeholder"
     };
 
-    var character = {
-        chID : 0,
-        chNAME : "placeholder",
-        chGAME : 0
-    };
-
     var genre = {
         genID : 0,
         genTITLE : "placeholder",
@@ -243,9 +300,34 @@ $("#createGameForm").submit(function (event) {
     }
 
     alert(game.gameNAME);
+    alert(getCharacter());
 
 });
 
+function getCharacterList (){
+    var characterList = [];
+    row11counter = 0;
+    while ($("#row11Name-" + row11counter).val() !== undefined) {
+        if ($("#row11Name-" + row11counter).val() === ""){
+            continue;
+        }else {
+            var character = {
+                chID : 0,
+                chNAME : $("#row11Name-"+row11counter),
+                chPFP : $("#row11URL-"+row11counter),
+                chGAME : null
+            };
+        }
+        row11counter++;
+    }
+}
+
+function generateRandomID(){
+    var dateObjekt = new Date();
+    var milliseconds = dateObjekt.getTime();
+
+    return milliseconds;
+}
 //FØRSTE VERSION AF CREATE GAME FUNKTIONALITETEN. SAT PÅ PAUSE FORDI DET VAR KOMPLICERET
 /*$("#createGameForm").submit(function (event) {
     event.preventDefault();
