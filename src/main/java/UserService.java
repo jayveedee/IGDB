2f8d7ecea5a7163e86307c0e8383f2834a648484
Asama <asamahayder@yahoo.dk>
2019-06-18 15:01:29 +0200

    addede at brugeren får begge sine roller når han bliver created, og at man nu ikke får en underlig alert når man creater en user

--- src/main/java/UserService.java
+++ src/main/java/UserService.java
@@ -15,18 +15,23 @@ public class UserService {
         this.mySQL=mySQL;
     }
 
-    //FIXME husk at gøre så useren bliver tildelt to roller som allerede eksisterer i databsen. nemlig: user og editor. Ved tilfælde af ban, mister useren sin editor-rolle (som står for redigering er artikler) men beholder user-rollen (som består af profil osv.)
+
     public boolean createUser(String username, String email, String password ){
         IUserDAO userDAO = new UserDAO(mySQL);
         IRoleDAO roleDAO = new RoleDAO(mySQL);
         UserDTO user = new UserDTO();
         RoleDTO role = new RoleDTO();
+        RoleDTO role1 = new RoleDTO();
+
+        role1.setRoleID(1);
+        role1.setRoleNAME("Editor");
 
         role.setRoleID(2);
         role.setRoleNAME("User");
 
         ArrayList<RoleDTO> roleList = new ArrayList<>();
         roleList.add(role);
+        roleList.add(role1);
 
         user.setUserEMAIL(email);
         user.setUserNAME(username);
